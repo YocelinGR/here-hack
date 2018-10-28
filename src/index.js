91cf90879f789b52eaee4bf16a10bab88ad6ee5f
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-10-28 17:08:58 -0600

    Trying with a fixed German located HERE map

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Location from './Location';
+import Map from './Map';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Map app_id="0MOsfImUqdHKOEjditV6" app_code="L8wyCqwmQMcsDAoPzn-JdA" />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
