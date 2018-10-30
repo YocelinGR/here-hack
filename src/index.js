d5adbc8a4a2d69de8438820b1f1a11073c1bbd3d
Judith <baruki110214@gmail.com>
2018-10-29 22:32:58 -0600

    creando calendario selección multiple

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import App from './App';
 import Location from './Location';
 import Map from './Map';
 import * as serviceWorker from './serviceWorker';
-import Lector from './Component/Lector/LectorQR';
-ReactDOM.render(<Lector />, document.getElementById('root'));
+import Calendario from './Component/Calendario';
+
+
+ReactDOM.render(<Calendario/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
