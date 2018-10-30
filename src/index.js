dcef261dfe007e61f9a38c8395a5d8b96c599b88
Judith <baruki110214@gmail.com>
2018-10-30 02:48:57 -0600

    consumiendo API Here formulario dirección

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Location from './Location';
-import Map from './Map';
+import AddressForm from './Component/Mapa/AddressForm';
 import * as serviceWorker from './serviceWorker';
 import Calendario from './Component/Calendario';
 
 
-ReactDOM.render(<Calendario/>, document.getElementById('root'));
+ReactDOM.render(<AddressForm/>, document.getElementById('root'));
 
 serviceWorker.unregister();
+
