20b46ae9f26eb5911723c586ab7474fbd9b99126
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-10-30 11:09:20 -0600

    Adding the splash page

--- src/Component/SplashScreen/SplashScreen.js
+++ src/Component/SplashScreen/SplashScreen.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './SplashScreen.css';
 
 const SplashScreen = () => {
     return(
         <div>
             <div className= "container fluid">
-                <hr className= "vertical-line"/>
-                <hr className= "vertical-line left"/>
-                <img src="../../assets/bus.png" alt="bus-icon" />
-                <h1>School Security</h1>
+                <div className= "vertical-line"></div>
+                <div className= "vertical-line-right"></div>
+                <div className= "vertical-line-left"></div>
+                <div className= "vertical-line-ligth"></div>
+                <img className= "bus-img" src="https://github.com/YocelinGR/here-hack/blob/master/src/assets/bus.png?raw=true" alt="bus-icon" />
+                <h1 className= "letter">School Security</h1>
             </div>
         </div>
     );
