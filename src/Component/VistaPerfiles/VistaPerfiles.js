9d4aaf8d134ffd60924ac52486cc601d940e83b0
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-10-30 12:11:29 -0600

    Adding the auth page (father or choffer)

--- src/Component/VistaPerfiles/VistaPerfiles.js
+++ src/Component/VistaPerfiles/VistaPerfiles.js
@@ -6,21 +6,20 @@ class VistaPerfiles extends Component {
     render () {
         return (
         <div className="container">
+            <div className= "container-fluid">
+                <div className= "vertical-line"></div>
+                <div className= "vertical-line-right"></div>
+                <div className= "vertical-line-left"></div>
+                <div className= "vertical-line-ligth"></div>
+                <img className= "bus-img" src="https://github.com/YocelinGR/here-hack/blob/master/src/assets/bus.png?raw=true" alt="bus-icon" />
+                <h1 className= "letter">School Security</h1>
+            </div>
             <div className="row">
                 <div className="row">
-                    <a href= "btns" className= "btn-perfil" id="conductor"><Link to='/VistaConductor'>Conductor</Link></a>
+                    <a href= "btn-padre" className= "btn-padre" id="padre">Padre de familia</a>
                 </div>
                 <div className="row">
-                <a href= "btns" className= "btn-perfil" id="acompañante"><Link to='/VistaAcompañante'>Acompañante</Link></a>
-                </div>
-                <div className="row">
-                <a href= "btns" className= "btn-perfil" id="padre"><Link to='/VistaPadre'>Padre</Link></a>
-                </div>
-                <div className="row">
-                <a href= "btns" className= "btn-perfil" id="escuela"><Link to='/VistaEscuela'>Escuela</Link></a>
-                </div>
-                <div className="row">
-                <a href= "btns" className= "btn-prperfilpfile" id="administrador"><Link to='/VistaAdmin'>Padre</Link></a>
+                <a href= "btn-conductor" className= "btn-conductor" id="conductor">Conductor</a>
                 </div>
             </div>
         </div>
