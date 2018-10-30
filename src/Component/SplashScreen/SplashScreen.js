2fc875595a05bcea7f6db59e717f322720caf826
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-10-30 08:33:40 -0600

    adding images

---
+++ src/Component/SplashScreen/SplashScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SplashScreen = () => {
+    return(
+        <div>
+            <div className= "container fluid">
+                <hr className= "vertical-line"/>
+                <hr className= "vertical-line left"/>
+                <img src="../../assets/bus.png" alt="bus-icon" />
+                <h1>School Security</h1>
+            </div>
+        </div>
+    );
+}
+
+export default SplashScreen;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import App from './App.js';
+import SplashScreen from './Component/SplashScreen/SplashScreen';
 import LoginPassword from './Component/LoginPassword/LoginPassword.js';
 
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<LoginPassword />, document.getElementById('root'));
+ReactDOM.render(<SplashScreen />, document.getElementById('root'));
 registerServiceWorker();
 
 
